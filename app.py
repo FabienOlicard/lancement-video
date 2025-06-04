8518aeb1a1ea49da25d2577bb5493179608ad798
Fabien Olicard <sherlocksmind@macbook-pro-de-fabien.home>
2025-06-04 13:51:51 +0200

    Expose Flask app for Render

--- app.py
+++ app.py
@@ -243,6 +243,7 @@ def fin():
     '''
 
 
+# ✅ Ligne modifiée pour Render ici :
 if __name__ == '__main__':
-    print("➡️ Flask démarre sur http://127.0.0.1:5000")
-    app.run(debug=True)
+    print("➡️ Flask démarre sur http://0.0.0.0:5000")
+    app.run(debug=True, host="0.0.0.0", port=5000)
